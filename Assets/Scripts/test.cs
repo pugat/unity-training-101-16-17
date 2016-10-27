8007b93735f443e4ca6301865d39d1afbebc7ec1
YunusBALCI <ComEnYns@outlook.com>
2016-10-27 18:12:11 +0300

    25 10 2016

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	public int _tamSayi = 3;
+	public float _noktaliSayi = 0.3f;
+	public string _karakter = "ieeepaucs";
+	public GameObject _obje;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Update");
+	}
+}
